72b97e76d907befc82a75ff937e83299a1bb0eac
SanketAnnadate <sanketannadatejain@gmail.com>
2022-05-14 00:19:32 +0530

    Eprime

--- Math/7PriemNumber.cpp
+++ Math/7PriemNumber.cpp
@@ -11,7 +11,19 @@ bool isPrime(int a)
     }
     return true;
 }
+
+bool isEPrime(int n){
+    if(n==1 || n%2==0)return false;
+    for (int i = 2; i*i <= n; i++)
+    {
+        if(n%i==0)    
+            return false;
+    }
+    return true;
+}
+
 int main()
 {
     cout << "Prime Number  " << isPrime(27) << endl;
+    cout << "Efficient Prime Number " <<isEPrime(97) << endl;
 }
