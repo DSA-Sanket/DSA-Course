c3fdac5345bba24075d8c3c8d5191b64be47ac77
SanketAnnadate <sanketannadatejain@gmail.com>
2022-05-14 00:45:14 +0530

    MEPrime Nuimer

--- Math/7PriemNumber.cpp
+++ Math/7PriemNumber.cpp
@@ -13,7 +13,7 @@ bool isPrime(int a)
 }
 
 bool isEPrime(int n){
-    if(n==1 || n%2==0)return false;
+    if(n==1)return false;
     for (int i = 2; i*i <= n; i++)
     {
         if(n%i==0)    
@@ -22,8 +22,20 @@ bool isEPrime(int n){
     return true;
 }
 
+bool isMEPrime(int n)
+{
+    if (n==1 || n%2==0 || n%3==0)return false;
+    if(n==2 || n==3) return true;
+    for (int i = 5; i*i <= n; i+=6)
+    {
+        if (n%i==0 || n%(i+2)==0) return false;
+    }
+    return true;
+}
+
 int main()
 {
     cout << "Prime Number  " << isPrime(27) << endl;
     cout << "Efficient Prime Number " <<isEPrime(97) << endl;
+    cout << "More Efficient Prime Number "<<isMEPrime(89) << endl;
 }
